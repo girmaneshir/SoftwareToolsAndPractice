f512b40b64e2c7f01dc72ee294a50c48c976fbe0
Asad <Asad@gmail.com>
2019-01-10 03:13:29 +0300

    All in one

---
+++ course.java
@@ -0,0 +1,21 @@
+class public course{
+	private String title;
+	private Integer credithour;
+	public void setTitle(String n)
+	 {
+		 title=n;
+	 }
+	public String getTitle()
+	{
+		return title;
+	}
+	public Integer getCreditHour()
+	{
+		return credithour;
+	}
+	public void setCreitTitle(String credit)
+	 {
+		 credithour=credit;
+	 }
+}
+
---
+++ student.java
@@ -0,0 +1,14 @@
+class public student{
+	private String name;
+	private Integer age;
+	public void setName(String n)
+	 {
+		 name=n;
+	 }
+	public String getName()
+	{
+		return name;
+	}
+}
+	
+	
